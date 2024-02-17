90e28b3e497579fd5784dda176a87d3fd3f7d010
PhilippeMitch <philippemitch91@gmail.com>
2024-02-17 10:23:22 +0700

    Update Docker file

--- analytics/config.py
+++ analytics/config.py
@@ -10,6 +10,8 @@ db_host = os.environ.get("DB_HOST", "127.0.0.1")
 db_port = os.environ.get("DB_PORT", "5432")
 db_name = os.environ.get("DB_NAME", "postgres")
 
+print("password ", db_password)
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
 
